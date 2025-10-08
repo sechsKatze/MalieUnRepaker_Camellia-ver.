3797626898236cacc60c6aed61e96ded8cd31856
sechsKatze(ゼクスカッツエ) <omerta.korean0423@gmail.com>
2025-10-08 10:48:26 +0900

    Delete launcher_GUI directory

--- launcher_GUI/malie/__init__.py
+++
@@ -1,6 +0,0 @@
-from malie.malieunpack import *
-from malie.malierepack import *
-from malie.maliekeys import *
-from malie.imagemgf import *
-from malie.imagedzi import *
-from malie.camellia import *
